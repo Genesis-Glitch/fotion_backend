11642db7a404139baf1f3019950201be50c3f87a
Iwan Saputra <ngonar@gmail.com>
2024-10-09 16:15:01 +1300

    add events endpoint

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, current_app
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
+from db import get_db
 
 app = Flask(__name__)
+
+@app.route('/initdb')
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -13,6 +21,14 @@ def hello_world():  # put application's code here
 @app.route('/user/<name>', methods = ['GET'])
 def get_username(name):
     return name
+
+@app.route('/events', methods = ['GET'])
+def get_events():
+    conn = get_db()
+    events = conn.execute('SELECT * FROM event').fetchall()
+    conn.close()
+
+    return events
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -19,7 +19,7 @@ def init_db_command():
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
+            "genai.db",
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
